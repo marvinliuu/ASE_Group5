0393a7cce49a6074c3ee4a420ead90f85714fab4
saisailiao <lsysaisai@gmail.com>
2023-01-31 20:58:14 +0000

    23.01.31 import map API - Siyu Liao

--- testDisasterEvent/app/src/main/java/com/example/testdisasterevent/ui/home/HomeFragment.java
+++ testDisasterEvent/app/src/main/java/com/example/testdisasterevent/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.testdisasterevent.ui.home;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +10,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.testdisasterevent.R;
 import com.example.testdisasterevent.databinding.FragmentHomeBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
@@ -28,6 +37,20 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION }, 100);
+
+        SupportMapFragment mapFragment= (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment == null){
+            FragmentManager fm= getFragmentManager();
+            FragmentTransaction ft= fm.beginTransaction();
+            mapFragment= SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+
+
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -36,6 +59,21 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        /**
+         * 在这里传经纬度即可
+         */
+        LatLng sydney = new LatLng(-37.812439, 144.972755);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(sydney));
+
+
+
     }
 
     @Override
