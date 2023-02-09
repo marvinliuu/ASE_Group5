a92f95b64c5651fce56bd60aa88ae5421b461354
MengqiWWang <wangm8@tcd.ie>
2023-02-09 11:55:53 +0000

    23.02.09 Animation effect - Mengqi Wang

--- testDisasterEvent/app/src/main/java/com/example/testdisasterevent/HomeActivity.java
+++ testDisasterEvent/app/src/main/java/com/example/testdisasterevent/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.example.testdisasterevent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.graphics.Paint;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +26,12 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ConstraintLayout constraintLayout = findViewById(R.id.container);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
 
         final Button registerButton = binding.signUp;
         final TextView loginButton = binding.logIn;
